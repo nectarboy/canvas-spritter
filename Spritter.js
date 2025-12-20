844b10692ef164f65fe85d955a9d8c4838c411ae
nectarboy <59830504+nectarboy@users.noreply.github.com>
2025-12-20 14:57:20 +0200

    can now submit a buffer of 2d vertex positions

--- Spritter.js
+++ Spritter.js
@@ -1,18 +1,12 @@
 const vs = `
 @vertex
 fn main(
-  @builtin(vertex_index) VertexIndex : u32
+    @builtin(vertex_index) VertexIndex : u32,
+    @location(0) position : vec2f
 ) -> @builtin(position) vec4f {
-  var pos = array<vec2f, 3>(
-    vec2(0.0, 0.5),
-    vec2(-0.5, -0.5),
-    vec2(0.5, -0.5)
-  );
-
-  return vec4f(pos[VertexIndex], 0.0, 1.0);
+    return vec4f(position, 0.0, 1.0);
 }
 `;
-
 const fs = `
 @fragment
 fn main() -> @location(0) vec4f {
@@ -33,6 +27,12 @@ class Spritter {
         this.device = device;
         this.encoder = device.createCommandEncoder();
 
+        this.bufferStride = 4 * 2;
+        this.someBuffer = device.createBuffer({
+            size: 4096,
+            usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+        });
+
         this.pipeline = device.createRenderPipeline({
             label: 'spritter pipeline',
             layout: 'auto',
@@ -40,7 +40,20 @@ class Spritter {
                 module: device.createShaderModule({
                     label: 'vs',
                     code: vs
-                })
+                }),
+                buffers: [
+                    // vertex buffer
+                    {
+                        arrayStride: this.bufferStride,
+                        attributes: [
+                            {
+                                shaderLocation: 0,
+                                offset: 0,
+                                format: 'float32x2'
+                            }
+                        ]
+                    }
+                ]
             },
             fragment: {
                 module: device.createShaderModule({
@@ -77,9 +90,27 @@ class Spritter {
             ]
         };
 
+        const testVertexBuffer = new Float32Array([
+            0.0 + Math.random(), 0.5,
+            -0.5, -0.5,
+            0.5, -0.5,
+
+            Math.random(), Math.random(),
+            Math.random(), Math.random(),
+            Math.random(), Math.random()
+        ]);
+        this.device.queue.writeBuffer(
+            this.someBuffer,
+            0,
+            testVertexBuffer.buffer,
+            testVertexBuffer.byteOffset,
+            testVertexBuffer.byteLength
+        );
+
         const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
         passEncoder.setPipeline(this.pipeline);
-        passEncoder.draw(3);
+        passEncoder.setVertexBuffer(0, this.someBuffer);
+        passEncoder.draw(testVertexBuffer.length);
         passEncoder.end();
 
         this.device.queue.submit([commandEncoder.finish()]);
